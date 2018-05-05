315023d299ff0c0a87d123b9aeaccc5d7a3bf624
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-05-06 01:03:35 +0200

    Add perso.config.ini.php to ignore files

---
+++ 2_SITEX/phase_04/INC/db.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrien
+ * Date: 5/05/18
+ * Time: 19:35
+ */
+
+class Db
+{
+
+}
---
+++ 2_SITEX/phase_04/INC/template.login.inc.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrien
+ * Date: 5/05/18
+ * Time: 17:37
+ */
